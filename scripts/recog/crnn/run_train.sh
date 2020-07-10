f8c2bcd9529ebc309687c6f6c72d976393a28416
Davids929 <448627058@qq.com>
2020-07-10 17:52:32 +0800

    update

--- scripts/recog/crnn/run_train.sh
+++ scripts/recog/crnn/run_train.sh
@@ -4,4 +4,5 @@
 
 python train_crnn.py --network resnet --num-layers 34 --batch-size 100 --max-len 50 --fix-width 448 \
 --short-side 32 --gpus 1 --num-workers 8 --warmup-epochs 0 --dataset-name scene \
-#--resume ./checkpoint/receipt-resnet34-crnn_best.params #--export-model
+--lr 0.0001 --lr-decay 0.2 --lr-decay-epoch 60 \
+--resume ./checkpoint/scene-resnet34-crnn_0070_0.4506.params #--export-model
